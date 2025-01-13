ee60d7be63fe88c9a6f62e691dcf5b429d5be73e
siheon <rlatlgjs357@gmail.com>
2025-01-13 10:59:53 +0900

    데이터 오류 수정

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const bookmarkButton = document.getElementById('bookmark-button');
 
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZmU3ZjAzNjA5MmVkNjMwOTFlYjNhNzEzYzE0ZDE5MCIsIm5iZiI6MTczNjMxMzgzOS4zNTYsInN1YiI6IjY3N2UwYmVmMjAzN2ZmMDRjOTRlNTlmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pNxV2dUSWqGWsjcftHfvDtCYm65NSyuLRV8zzlPnEH0'
-    }
-};
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZmU3ZjAzNjA5MmVkNjMwOTFlYjNhNzEzYzE0ZDE5MCIsIm5iZiI6MTczNjMxMzgzOS4zNTYsInN1YiI6IjY3N2UwYmVmMjAzN2ZmMDRjOTRlNTlmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pNxV2dUSWqGWsjcftHfvDtCYm65NSyuLRV8zzlPnEH0'
+//     }
+// };
 
 const API_KEY = 'efe7f036092ed63091eb3a713c14d190';
 const API_URL = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`;
@@ -17,7 +17,7 @@ const API_URL = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&pag
 // TMDB API 영화 데이터 가져오기
 async function fetchMovies() {
     try {
-        const response = await fetch(API_URL, options);
+        const response = await fetch(API_URL);
         const data = await response.json();
         displayMovies(data.results);
     } catch (err) {
@@ -32,7 +32,7 @@ function displayMovies(movies){
         const movieCard = document.createElement('div'); // 새로운 div 생성
         movieCard.classList.add('movie-card'); // movie-card 클래스 추가
         movieCard.innerHTML = `
-        <div id="${id}" class="card">
+        <div class="card">
             <img class="card-img-top" src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">${movie.title}</h5>
